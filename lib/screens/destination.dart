d3d3ee7e0e742b1004aec06396e962f06deba817
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-08-20 22:08:58 +0100

    Nav

---
+++ lib/screens/destination.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:travel_ui/models/destination.dart';
+
+class DestinationScreen extends StatefulWidget {
+  final Destination destination;
+  const DestinationScreen({Key? key, required this.destination})
+      : super(key: key);
+
+  @override
+  _DestinationScreenState createState() => _DestinationScreenState();
+}
+
+class _DestinationScreenState extends State<DestinationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
